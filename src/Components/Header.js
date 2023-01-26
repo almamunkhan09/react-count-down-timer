805c83ea44c324a198794b05adae90c7bd96d17c
Al Mamun Khan <almamunkhan09@gmail.com>
2023-01-26 15:40:54 +0100

    Make Functionaly of the app

---
+++ src/Components/Header.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  // const [days, setDays] = useState(0);
+  // const [hours, setHours] = useState(0);
+  // const [minutes, setMinutes] = useState(0);
+  // const [sec, setSec] = useState(0);
+  const [timeDiff, setTimeDiff] = useState(0);
+  const [deadline, setDeadline] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    const timerInt =
+      timeDiff > 0 &&
+      setInterval(() => {
+        setTimeDiff(() => timeDiff - 1);
+        if (
+          Math.ceil(deadline / 1000) === Math.ceil(new Date().getTime() / 1000)
+        ) {
+          setIsFinished(true);
+        }
+      }, 1000);
+    return () => {
+      clearInterval(timerInt);
+    };
+  }, [timeDiff, deadline]);
+
+  const handlInput = (event) => {
+    setDeadline(new Date(event.target.value).getTime());
+  };
+  const handleStart = () => {
+    const milidiff = deadline - new Date().getTime();
+    const seconds = Math.floor(milidiff / 1000);
+    setTimeDiff(seconds);
+  };
+
+  // setSec(timeDiff % 60);
+
+  return (
+    <>
+      <div className=" flex text-white justify-between">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">Count Down</h1>
+        <ul className="flex p-4">
+          <li className="p-4"> Home</li>
+          <li className="p-4"> About</li>
+          <li className="p-4"> Contact</li>
+        </ul>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="datepicker relative form-floating mb-3 xl:w-96">
+          <input
+            type="datetime-local"
+            onChange={handlInput}
+            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="Select a date"
+          />
+          <label htmlFor="floatingInput" className="text-gray-700">
+            Select a date
+          </label>
+        </div>
+      </div>
+      <div className="flex   p-5 items-center justify-center gap-5 text-white">
+        <div>
+          <span className="countdown font-mono text-white text-4xl">
+            <span className="text-white" style={{ '--value': 15 }}>
+              {Math.floor(timeDiff / (60 * 60 * 24))}
+            </span>
+          </span>
+          days
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">
+            <span style={{ '--value': 10 }}>
+              {' '}
+              {Math.floor((timeDiff % (60 * 60 * 24)) / (60 * 60))}
+            </span>
+          </span>
+          hours
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">
+            <span style={{ '--value': 24 }}>
+              {' '}
+              {Math.floor((timeDiff % (60 * 60)) / 60)}
+            </span>
+          </span>
+          min
+        </div>
+        <div>
+          <span className="countdown font-mono text-4xl">
+            <span style={{ '--value': 44 }}>{timeDiff % 60}</span>
+          </span>
+          sec
+        </div>
+      </div>
+      <div className="flex spac-x-2 justify-center">
+        <div>
+          <button
+            onClick={handleStart}
+            type="button"
+            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Button
+          </button>
+        </div>
+      </div>
+
+      {isFinished ? <div className="text-white">Time Finished</div> : ''}
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+
+  theme: {
+    extend: {},
+    container: { center: true },
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+};
